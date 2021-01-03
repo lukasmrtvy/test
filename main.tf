b2b877269cae465c82476e5f738b92f5d5dc3e7b
Lukas M <lukas.mrtvy@gmail.com>
2021-01-03 23:06:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 resource "docker_image" "nginx" {
-  name = "nginx:1.7.8"
+  name = "lukasmrtvy/tools:latest@sha256:732d70b72f93b06fb202bd5c464d7b0e2c5c3fc6559456622af1535d64e79ea7"
 }
 
 module "consul" {
